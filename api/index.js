dbdf429dfe807721ccbc8a2d31af4edddf01b94e
PatoPon <novaesgui2323@outlook.com>
2025-06-13 08:29:58 -0300

    commit

--- api/index.js
+++ api/index.js
@@ -20,13 +20,8 @@ dotenv.config()
 
 const app = express()
 app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
-  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-  next();
-});
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/converted', express.static(path.resolve('converted')));
+app.use('/converted', cors({ origin: '*' }), express.static(path.resolve('converted')));
 app.use(express.json({ limit: '50mb' }));       // aumenta o limite para 50MB
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
